a8892cef2b54257f8f5c9728f908ae3aa08eb6a2
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2025-06-11 18:41:02 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 # Telegram Bot Configuration
 BOT_TOKEN = "7679518801:AAEGr2j6cQ-py-dAdM9sFEjlEzLaHsJYW9A"  # Replace with your bot token from BotFather
-API_ID = "12380656"  # Replace with your API ID from my.telegram.org
-API_HASH = "d927c13beaaf5110f25c505b7c071273"  # Replace with your API Hash from my.telegram.org
+API_ID = "26482250"  # Replace with your API ID from my.telegram.org
+API_HASH = "cd26ed6b35e6cef9e65e79ac6be4f7d4"  # Replace with your API Hash from my.telegram.org
 
 # MongoDB Configuration
 MONGODB_URI = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"  # Replace with your MongoDB connection URI (e.g., "mongodb+srv://user:pass@cluster.mongodb.net/yourdb")
