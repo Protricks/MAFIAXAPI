9950e0faba42c3890afa490003f103c321c84dc0
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2025-06-11 16:49:19 +0530

    Create config.py

---
+++ Bot/config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+MONGODB_URI = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_NAME = "ytapi_db"
+ADMIN_USER_ID = 123456789  # Replace with your Telegram ID
