eddea2fce48cc8828a8b03b2545be6ed20de4097
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2025-06-11 17:14:54 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,4 +1,13 @@
+# Get these from https://my.telegram.org
+API_ID = "12380656"  # Replace with your actual api_id
+API_HASH = "d927c13beaaf5110f25c505b7c071273"  # Replace with your actual api_hash
+
+# Get this from @BotFather after creating the bot
 BOT_TOKEN = "7679518801:AAEGr2j6cQ-py-dAdM9sFEjlEzLaHsJYW9A"
-MONGODB_URI = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"
-MONGODB_NAME = "ytapi_db"
-ADMIN_USER_ID = "6035523795"  # Replace with your Telegram ID
+
+# MongoDB setup
+MONGODB_URI = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"  # Or your full cloud URI
+MONGODB_NAME = "yt_api_bot"
+
+# Replace with your Telegram user ID (integer)
+ADMIN_USER_ID = "6035523795"  # Your Telegram user ID
